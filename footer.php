eba081855523902b6e88c0231a0090b614057ec4
Vanessa Naja <vanessacnaja@gmail.com>
2019-05-14 12:28:52 -0700

    initial commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package cvf
+ */
+
+?>
+		<!-- corner line artifact -->
+		<div class="corner-line"></div>
+	</div><!-- #content -->
+</div><!-- .top-content-container-->
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<p>&copy 2017 Codetalk Venice Festival</p>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
